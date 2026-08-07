7a2d41ed9df0f3a81d3c767a013f11c586abbc8c
Ashraf Ali <ashrafalistudy@gmail.com>
2026-08-07 22:50:06 +0530

    fixed redis loadenv

--- BACKEND/services/redis.py
+++ BACKEND/services/redis.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 import redis
 import os
+from dotenv import load_dotenv
 app = FastAPI()
+load_dotenv()
+
 
 
 _redis = redis.from_url(os.environ.get("REDIS_URL"),decode_responses=True)
